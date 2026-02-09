a0d6d100ae7e87fb9a22faf08cfd64b96c537267
Claudio Barrios <cbmne.93@gmail.com>
2026-02-09 19:52:21 -0300

    Migraciones categories y posts

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <h1 class="text-3xl font-bold underline text-blue-500">
+        Hola desde About
+    </h1>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <h1 class="text-3xl font-bold underline text-red-500">
+        Hola desde Contact
+    </h1>
+@endsection
