479cd134e666dca0082e3c0ac637d3d4f141d167
Claudio Barrios <cbmne.93@gmail.com>
2026-02-09 21:40:02 -0300

    Creando Modelos y Controladores

--- resources/views/contact.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-
-@section('content')
-    <h1 class="text-3xl font-bold underline text-red-500">
-        Hola desde Contact
-    </h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,16 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Dashboard\CategoryController;
+use App\Http\Controllers\Dashboard\PostController;
 use Illuminate\Support\Facades\Route;
 
 
 
-/* Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-Route::get('/about', function () {
-    return view('about');
-})->name('about'); */
 
 Route::resource('categories', CategoryController::class);
+Route::resource('posts', PostController::class);
